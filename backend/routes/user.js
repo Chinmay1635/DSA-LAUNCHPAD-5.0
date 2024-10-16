b51fe6e550f944caa75da6b4c9fc18fc827b659b
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-10-16 15:09:29 +0530

    Added update score route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 
 const {loginUser, registerUser} = require('../controllers/userControl');
 const { isLoggedIn } = require('../middlewares/isLoggedIn');
+const { updateScore } = require('../controllers/scoreControl');
+const userModel = require('../models/userModel');
 
 router.get("/", function(req,res){
     res.json("Welcome to DSA Launchpad 5.0");
@@ -12,5 +14,7 @@ router.get("/", function(req,res){
 router.post("/registerUser", registerUser);
 
 router.post("/loginuser", loginUser);
+
+router.post("/update-score", isLoggedIn, updateScore)
 
 module.exports = router;
