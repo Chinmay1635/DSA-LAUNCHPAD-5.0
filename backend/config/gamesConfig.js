3b53175ea154b47b2f817000f74442c21d95cc0c
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-10-16 15:06:41 +0530

    Added predefined game and initialized all levels with 0 score

---
+++ backend/config/gamesConfig.js
@@ -0,0 +1,21 @@
+const predefinedGames = [
+    {
+        game: "BST Builder",
+        levels: [
+            { level: 1, score: 0 },
+            { level: 2, score: 0 },
+            { level: 3, score: 0 }
+        ]
+    },
+    {
+        game: "Sorting Game",
+        levels: [
+            { level: 1, score: 0 },
+            { level: 2, score: 0 },
+            { level: 3, score: 0 }
+        ]
+    },
+    // Add more games and levels here
+];
+
+module.exports = predefinedGames;
