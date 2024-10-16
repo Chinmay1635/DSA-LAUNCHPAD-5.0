2dcc233dff7ad49a0f9724e960b2ab74fe5c6919
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-10-16 15:08:38 +0530

    Updated the middleware for storing email in cookies

--- backend/middlewares/isLoggedIn.js
+++ backend/middlewares/isLoggedIn.js
@@ -5,12 +5,19 @@ module.exports.isLoggedIn = async (req,res,next)=>{
     if(req.cookies.token){
         try {
             const data = jwt.verify(req.cookies.token, process.env.JWT_KEY);
-            req.user = await userModel.findOne({email:data}).select("-password");
-            next();
+            // Fetch the user email from the token
+            const user = await userModel.findOne({ email: data.email });
+
+            if (user) {
+                req.email = user.email; // Attach email to req object
+                next(); // Proceed to the next middleware or route handler
+            } else {
+                res.status(401).json({ message: "User not found" });
+            }
         } catch (error) {
-            res.json(error.messeage)
+            res.json({error: error.messeage})
         }
     }else{
-        res.json("You are not autherised");
+        res.json({message: "You are not autherised"});
     }
 }
